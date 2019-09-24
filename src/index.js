0b335726a62bd898aaa60a36f45b6fada588a307
Mohamed <fofmak26@gmail.com>
2019-09-24 22:12:29 +0000

    Creates and provides the state to 'App' and its     components using redux and react-redux.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(todoApp);
+
+ReactDOM.render(
+<Provider store={store}>
+   <App /> 
+</Provider>, 
+document.getElementById('root'));
 
